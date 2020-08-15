2d55ce761c6700622a86fffd6cc3ccc40f177d13
YaoKevinn <vasty85817@gmail.com>
2020-08-15 05:48:48 -0300

    Updates

---
+++ precache-manifest.0db99190688a35b034d8a4c510bdd83d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e76c0d815a7994fd1e8984896c0daa",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "b6ef8ac16844fb98fc6c",
+    "url": "/amazon-clone/static/css/main.19f8b95b.chunk.css"
+  },
+  {
+    "revision": "d81bffd1cb4e285b1e31",
+    "url": "/amazon-clone/static/js/2.2bdb52d2.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/amazon-clone/static/js/2.2bdb52d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6ef8ac16844fb98fc6c",
+    "url": "/amazon-clone/static/js/main.327ae414.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
